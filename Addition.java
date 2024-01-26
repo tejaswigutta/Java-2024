624ecf3cbb5dfeb630d93e5adb312febae1ba8db
Tejaswi Gutta <tejaswigutta788@gmail.com>
2024-01-26 15:19:04 -0500

    Added Addition.java

---
+++ Addition.java
@@ -0,0 +1,134 @@
+public class Addition {
+
+  
+  public static void main(String[] args) {
+    
+       
+       //0-99
+       //1-100
+       //11-111
+       //99-199
+
+       //loops - for,while,do while
+       /*for(initilization;condition;increment/decrement/update){
+
+       }*/
+
+       /*initilization
+       while(condition){
+        //
+        //
+        //
+        updation
+       }*/
+
+       /*int count=1;
+       while(true){
+        System.out.println(count+" Tejaswi");
+        count++;
+       }*/
+
+       //do while
+
+       //initilization->condition->enters while loop -> updation
+
+       /*initilization
+       do{
+        //
+        ///
+        updation
+       }while(condition);
+       /// */
+
+       /*int c=0;
+       do{
+        System.out.println(c+1+" tejaswi");
+        //c++;
+       }while(c<=9);*/
+
+       //c=2
+
+       /*for(i,c,u){
+        for(i,c,u){
+
+        }
+       }*/
+
+       for(int i=1;i<=3;i++){
+        for(int j=1;j<=10;j++){
+          System.out.println(i+" "+j);
+        }
+        ////
+       }
+
+       //initilization->condition->enters for loop -> updation
+
+      i=4  j=4
+      i=1 
+      i=2
+      i=3
+      1 1
+      1 2
+      1 3
+      2 1
+      2 2
+      2 3
+      3 1
+      3 2
+      3 3
+      
+
+       /*1 1
+       1 2
+       1 3
+       2 1
+       2 2 
+       2 3
+       3 1
+       3 2 
+       3 3*/
+
+       
+
+       
+       /*for(int i=1;i<=100;i=i+1){
+        System.out.println(i+" tejaswi");
+        System.out.println("thank you");
+       }*/
+
+
+       //initilization ->condition->inside the loop->updation
+       
+      
+    }
+}
+
+/*1 2 3 good
+4 5 6 bad
+invalid*/
+//dayOfTheWeek -> 1-sunday 2 monday 3 tuesday
+
+/*A=90-100
+B-80-89
+C-70-79
+D-60-69
+fail <60*/
+//datatype variable=condition?optionA:optionB;
+//false 20 49
+//int a=5;
+
+//isEligible =false
+//5==5
+//=,==
+//= - assignment operator
+//== - equality
+//!= - not equals
+
+
+
+
+//a++ //post incrementation
+//++a //pre incrementation
+
+//a=3
+
